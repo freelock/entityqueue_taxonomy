20452a6dc9b4e75bc7fef74342deb7e15d2cb5d4
John Locke <john@freelock.com>
2018-01-23 21:35:46 -0800

    Initial D8 entityqueue_taxonomy module

---
+++ src/Plugin/views/argument_default/QueueNameTerm.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\entityqueue_taxonomy\Plugin\views\argument_default;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\entityqueue\Entity\EntityQueue;
+use Drupal\taxonomy\Plugin\views\argument_default\Tid;
+
+
+/**
+ * @ViewsArgumentDefault(
+ *  id = "entityqueue_name_term",
+ *  title = @Translation("EntitySubqueue name from Term ID"),
+ * )
+ */
+class QueueNameTerm extends Tid {
+
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+    // We cannot handle multiple terms...
+    unset($form['anyall']);
+
+    // Add list of queues.
+    $queues = EntityQueue::loadMultiple();
+    $options = [];
+    foreach ($queues as $queue) {
+      if ($queue->getHandler() == 'taxonomy_term') {
+        $options[$queue->id()] = $queue->label();
+      }
+    }
+    $form['entityqueue'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Taxonomy EntityQueue ID'),
+      '#default_value' => $this->options['entityqueue'],
+      '#options' => $options,
+    ];
+  }
+
+  public function defineOptions() {
+    $options = parent::defineOptions();
+    // don't need anyall
+    unset ($options['anyall']);
+    // do need queue name
+    $options['entityqueue'] = ['default' => null];
+
+    return $options;
+  }
+
+  public function getArgument() {
+    $tid = parent::getArgument();
+    $queue = $this->options['entityqueue'];
+    return $queue . '_' . $tid;
+  }
+}
